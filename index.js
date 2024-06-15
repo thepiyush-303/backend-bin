235c053a00523266823a9e268286d90b72e5ecf7
unknown <2023kuec2027@iiitkota.ac.in>
2024-06-15 22:43:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/hello',(req,res) =>{
+  res.send('<h4>say hello</h4>')
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${process.env.port}`)
+})
+
+// dot env chaiye kyuke agar isse kahi deploy kroge to port number or bhibhot kuch vo he transfer hojega jo ke shayad uske device me vo port available na ho isliye .env file chaiye
